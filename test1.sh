d41a48d80879afb3ae8490b25d7305fd614c4ada
Billy Li <egoistheresy@outlook.com>
2022-11-01 20:08:10 -0500

    changed the test.sh file

--- test1.sh
+++ test1.sh
@@ -3,4 +3,10 @@ gcc -g psort.c -o psort
 mergeSort ./inputfiles/input1.bin 
 psort ./inputfiles/input1.bin
 diff output1.bin output2.bin
+ret=$?
+if [[ $ret -eq 0 ]]; then
+    echo "passed."
+else
+    echo "failed."
+fi
 rm -r output1.bin output2.bin
