321d076c98e865796f27bb5e436023770a5651f2
Yixuan Ye <yixuan@royal-26.cs.wisc.edu>
2022-11-05 15:55:35 -0500

    multi-thread readin

--- psort.c
+++ psort.c
@@ -27,15 +27,18 @@ struct arrsects{
 };
 
 struct readinarg {
+    int section;
     int *ptr;
     int* keys_arr;
     int** following_darr;
     int start;
     int radius;
+    int num_threads;
 };
 
 struct map myMap; // global myMap variable
 int current_finished_threads;
+int readin_finished_threads;
 // mutex: wait for current level child threads
 pthread_cond_t condition_wait = PTHREAD_COND_INITIALIZER;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
@@ -79,24 +82,30 @@ printErrMsg(char* msg) {
 void* readin_helper(void* args)
 {
     struct readinarg* argptr = (struct readinarg*) args;
+    //int section = argptr->section;
     int *ptr = argptr->ptr;
     int* keys_arr = argptr->keys_arr;
     int** following_darr = argptr->following_darr;
     int start = argptr->start;
     int radius = argptr->radius;
+    int num_threads = argptr->num_threads;
     
     for(int i = 0; i < radius; i ++)
     {
-        int key = ptr[start + i];
-        keys_arr[(start + i) / 25] = key;
-        start++;
+        int key = ptr[start + i * 25];
+        keys_arr[start / 25 + i] = key;
         int* following = malloc(24 * sizeof(int));
         for (int j = 0; j < 96/sizeof(int); j ++) {
-            following[j] = ptr[(start + i) + j];
+            following[j] = ptr[start + i * 25 + j + 1];
         }
-        following_darr[(start + i) / 25] = following;
-        start += 23;
+        following_darr[start / 25 + i] = following;
     }
+    pthread_mutex_lock(&lock);
+    readin_finished_threads++;
+    // printf("now finished: %d, total: %d\n", readin_finished_threads, num_threads);
+    if (readin_finished_threads >= num_threads)
+        pthread_cond_signal(&condition_wait);
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
@@ -128,37 +137,48 @@ readin(const char* filename)
     int** following_darr = malloc(rc_no * sizeof(int*)); // each 96-byte followings
     
     int num_thread = get_nprocs();
-    int num_chunk = size / 25;
+    int num_chunk = size / 100;
     num_thread = num_thread > num_chunk ? num_chunk : num_thread;
-    int fully_modulus = (size / 25) % num_thread;
-    int radius = (size / 25) / num_thread;
+    int remain_block = (size / 100) % num_thread;
+    int radius = (size / 100) / num_thread;
 
     pthread_t threads[num_thread];                       // thread lists
     struct readinarg args_arr[num_thread];   
 
     for(int i = 0; i < num_thread; i ++)
     {
-        int rc = pthread_create(&threads[i], NULL, thread_merge_divide, (void*)(&args_arr[i]));
+        args_arr[i].section = i;
+        args_arr[i].ptr = ptr;
+        args_arr[i].keys_arr = keys_arr;
+        args_arr[i].following_darr = following_darr;
+        args_arr[i].start = i * 25 * radius;
+        args_arr[i].radius = radius;
+        args_arr[i].num_threads = num_thread;
+        int rc = pthread_create(&threads[i], NULL, readin_helper, (void*)(&args_arr[i]));
         if(rc) {
             printErrMsg("Error while creating threads");
             exit(1);
         }
     }
-    if (fully_modulus > 0) {
-        int rest = size - radius * (num_thread * 25);
-        int rest_start = 123;
-        for(int i = 0; i < radius; i ++)
+    pthread_mutex_lock(&lock);
+    while (readin_finished_threads < num_thread)
+        pthread_cond_wait(&condition_wait, &lock);
+    pthread_mutex_unlock(&lock);
+    if (remain_block > 0) {
+        int start_point = radius * num_thread * 25;
+        int start_index = radius * num_thread;
+        for(int i = 0; i < remain_block; i ++)
         {
-            int key = ptr[start + i];
-            keys_arr[(start + i) / 25] = key;
+            int key = ptr[start_point + i * 25];
+            keys_arr[start_index + i] = key;
             int* following = malloc(24 * sizeof(int));
             for (int j = 0; j < 96/sizeof(int); j ++) {
-                following[j] = ptr[(start + i) + j];
+                following[j] = ptr[start_point + i * 25 + j + 1];
             }
-            following_darr[(start + i) / 25] = following;
+            following_darr[start_index + i] = following;
         }
     }
-      
+    
     myMap.length = size / 100;
     myMap.keys = keys_arr;
     myMap.followings = following_darr;
@@ -232,6 +252,7 @@ merge(struct map * myMap, int left, int mid, int right, int num_level_threads) {
     }
     pthread_mutex_lock(&lock);
     current_finished_threads++;
+    
     if (current_finished_threads >= num_level_threads)
         pthread_cond_signal(&condition_wait);
     pthread_mutex_unlock(&lock);
@@ -388,16 +409,16 @@ int
 main(int argc, char const *argv[])
 {
     const char* filename = argv[1];
-    //const char* filename = "inputfiles/8.in";
+    //const char* filename = "inputfiles/9.in";
     const char* output1 = argv[2];
     //const char* output1 = "inputfiles/8.out";
 
     //initialize mymap
     readin(filename);
-    // printMap(&myMap);
-    // printf("之后\n");
+    //printMap(&myMap);
+    //printf("之后\n");
     mt_thread_sort();
     writeOut(output1, &myMap);
-    // printMap(&myMap);
+    //printMap(&myMap);
     return 0;
 }
